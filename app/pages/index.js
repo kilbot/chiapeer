b18c9af70961fa2fd079760194247fe0b01babb5
kilbot <paul@kilbot.com.au>
2021-04-20 15:35:36 +0200

    add frontend

---
+++ app/pages/index.js
@@ -0,0 +1,57 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Chia Peer</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://chiapeer.com/">Chia Peer</a>
+        </h1>
+
+        <p className={styles.description}>
+          Open peer for the <a href="https://chia.net/">Chia</a> Blockchain
+        </p>
+        <p className={styles.description}>
+          Add <code className={styles.code}>chiapeer.com:8444</code> via the GUI<br /> or use the command
+          <code className={styles.code}>chia show -a chiapeer.com:8444</code>
+        </p>
+
+        {/* <div className={styles.grid}>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h3>Learn &rarr;</h3>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
+
+          <a
+            href="https://github.com/vercel/next.js/tree/master/examples"
+            className={styles.card}
+          >
+            <h3>Examples &rarr;</h3>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
+
+          <a
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+          >
+            <h3>Deploy &rarr;</h3>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+        </div> */}
+      </main>
+    </div>
+  )
+}
